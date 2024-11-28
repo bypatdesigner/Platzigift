ed8554794a4c61b8f36b5dd6060f046989816115
Jeanny Patricia Garcia <patgarcia.abafg@gmail.com>
2024-11-28 18:57:09 +0100

    28.11

--- functions.php
+++ functions.php
@@ -20,7 +20,7 @@ function assets(){
     wp_enqueue_script('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js', array('jquery','popper'), '5.3.3', true);
 
 
-
+    wp_enqueue_script('custom',get_template_directory_uri().'/assets/js/custom.js', array(), '1.0', true);
 }
 
 add_action( 'wp_enqueue_scripts', 'assets');
