0ca5a1d492bcfea127427e85f4ed9b973dde8b8a
Jeanny Patricia Garcia <patgarcia.abafg@gmail.com>
2024-11-29 15:34:14 +0100

    29.11

--- functions.php
+++ functions.php
@@ -3,22 +3,27 @@
 function init_template(){
     add_theme_support('post-thumbnails');
     add_theme_support( 'title-tag' );
-}
 
+    register_nav_menus(
+        array( 'top_menu' => 'Menu principal')
+    );
+}
 
 add_action( 'after_setup_theme','init_template'  ); 
 
 function assets(){
-    wp_register_style( 'bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css', array(), '5.3.3', 'all' );
+   
+    wp_register_style( 'bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css', array(), '4.5.2', 'all' );
 
-    wp_register_style( 'monserrat', 'https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap', array(), '1.0', 'all' );
+    wp_register_style( 'Montserrat', 'https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap', array(), '1.0', 'all' );
      
-    wp_enqueue_style( 'estilos', get_stylesheet_uri(), array(), '1.0', 'all' );
+    wp_enqueue_style( 'estilos', get_stylesheet_uri(), array('bootstrap', 'Montserrat'), '1.0', 'all' );
+
+    wp_enqueue_style( 'estilos1', get_stylesheet_uri(), array(), '1.0', 'all' );
 
     wp_register_script('popper', 'https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js',  array(), '2.11.8', true);
     
-    wp_enqueue_script('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js', array('jquery','popper'), '5.3.3', true);
-
+    wp_enqueue_script('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@4.5.2/dist/js/bootstrap.min.js', array('jquery','popper'), '5.3.3', true);
 
     wp_enqueue_script('custom',get_template_directory_uri().'/assets/js/custom.js', array(), '1.0', true);
 }
--- header.php
+++ header.php
@@ -4,17 +4,26 @@
     <meta charset='utf-8'>
     <meta http-equiv='X-UA-Compatible' content='IE=edge'>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
+    
     <?php wp_head() ?>
 </head>
 <body>
 
 <header>
     <div class="container">
-      <div class="row">
-        <div class="col-4">
+      <div class="row align-items-center">
+          <div class="col-4">
             <img src="<?php echo get_template_directory_uri()?>/assets/img/logo.png" alt="logo" />
-        </div>
-        
+          </div>
+          <div class="col-8">
+              <nav>
+              <?php wp_nav_menu( 
+                array('theme_location' => 'top_menu', 
+                            'menu_class' => 'menu_principal',
+                            'container_class'=> 'container_menu' ) 
+                            );?>
+              </nav>
+          </div>
       </div>
     </div>
 
